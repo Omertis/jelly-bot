0e92707454b7ef1ce425325375f3bc36c788c573
Omertis <36013948+Omertis@users.noreply.github.com>
2018-04-21 20:23:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2467,8 +2467,7 @@ Server MemberCount : **${gmemb} **
 
  client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "#";
-    if(message.content.startsWith(prefix + 'bdc')) {
+    if(message.content == ('bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
