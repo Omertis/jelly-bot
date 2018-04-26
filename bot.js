5e16e854001792734c800960acbf19aa0d9b069d
Omertis <36013948+Omertis@users.noreply.github.com>
2018-04-26 15:30:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ fetchAllMembers: true });
+const moment = require("moment");
 
 client.on('message', message => {
     var prefix = ".";
