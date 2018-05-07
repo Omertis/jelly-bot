0cadc56ddaf052b19f50deb1cb87c6cb1d61c10e
Omertis <36013948+Omertis@users.noreply.github.com>
2018-05-08 00:14:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -663,7 +663,20 @@ message.channel.sendMessage("", {embed: {
 
 });                           
 //------------------------------------------------------------------------                           
-                           
+const adminprefix = ".";
+const devs = ['426471752877604874'];
+
+client.on('message', message => {
+if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
+        console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`تم اعادة تشغيل البوت`);
+    }
+  
+  });                           
                            
                            
                            
