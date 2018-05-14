ce2abc6fe3fe76851b70de3de36b5c07afefe9e1
Omertis <36013948+Omertis@users.noreply.github.com>
2018-05-14 09:46:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -764,12 +764,7 @@ client.on('ready', () => {
 });
 
 
-const Music = require('discord.js-musicbot-addon');
-const music = new Music(client, {
-    prefix: ".", 
-    youtubeKey: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
-  
-  });
+
 
 
 client.login(process.env.BOT_TOKEN);
